d597f64518616f471bcdcecf01b11c2505c19e57
Kevin Fichou <kevinfichou@gmail.com>
2020-12-17 22:51:51 +0100

    deuxieme commit pour creer une nouvelle branche

---
+++ playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4f, rot = 80f, curSpeed;
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.LeftControl)) {
+            curSpeed = speed * 2;
+        }
+        else{
+            curSpeed = speed;
+        }
+
+        transform.Rotate(Vector3.up * rot * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
+
+        /*float goForward = Input.GetAxis("Vertical");
+        if(goForward>0)
+        {
+        transform.Translate(Vector3.forward * curSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical")); 
+        }*/
+        transform.Translate(Vector3.forward * curSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"));
+        
+    }
+}
