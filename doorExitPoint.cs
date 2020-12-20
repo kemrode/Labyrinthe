67d14ced8598a650b7012bec5091a32bac04c08a
Kevin Fichou <kevinfichou@gmail.com>
2020-12-20 22:43:21 +0100

    doorExitPoint script began 20122020 by debug.log to verify if it's OK and it is

---
+++ doorExitPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorExitPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+        Debug.Log(other.name + " est à l'intérieur du trigger");
+
+        }
+    }
+
+}
