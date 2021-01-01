2b5ef96cae4409669b7bd8513ebe98fbd09a63f2
Kevin Fichou <kevinfichou@gmail.com>
2021-01-01 18:38:11 +0100

    final Level 1 befor to build Level 2

--- DoorScript.cs
+++ DoorScript.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
 
 public class DoorScript : MonoBehaviour
 {
     public bool CanOpen = false;
+
+    [SerializeField]
+    AudioClip sndOpen, sndDenied;
+
+    private AudioSource myAudioSource;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag=="Player" && CanOpen)
         {
             GetComponent<Animator>().enabled = true;
+            myAudioSource.PlayOneShot(sndOpen);
+        }
+        else
+        {
+            myAudioSource.PlayOneShot(sndDenied);
         }
     }
 }
