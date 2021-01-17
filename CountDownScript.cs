736f1c466f1598d068dd8e1f172d18057176f4ae
Kevin Fichou <kevinfichou@gmail.com>
2021-01-17 15:10:20 +0100

    added a UI menu scene with code to launch the game

---
+++ CountDownScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour
+{
+    [SerializeField]
+    private int startCountDown = 60;
+
+    [SerializeField]
+    Text TxtCountDown;
+
+
+    void Start()
+    {
+        TxtCountDown.text = "TimeLeft : "+startCountDown;
+        StartCoroutine(Pause());
+    }
+
+    IEnumerator Pause()
+    {
+        while (startCountDown > 0)
+        {
+            
+            yield return new WaitForSeconds(1f);
+            startCountDown --;
+            TxtCountDown.text="TimeLeft :  "+ startCountDown;
+        }
+
+        Debug.Log("La mort vous étreint");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ UIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01");
+
+    }
+}
